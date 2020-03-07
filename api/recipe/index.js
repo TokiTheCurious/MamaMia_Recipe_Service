7e26ed35abc8f0bbaa3f02f78898a90b4c276c55
toki monkey <tokithemonkey@gmail.com>
2020-03-07 20:07:43 +0900

    remove unused functions

--- api/recipe/index.js
+++ api/recipe/index.js
@@ -23,33 +23,3 @@ recipeGetRequests = async (req, res) => {
     }
     return recipeService.getRecipes(req, res)
 }
-
-
-getRecipeById = async (req, res) => {
-    await Recipe.findOne({ _id: req.query.id }, (err, recipe) => {
-        if (err){
-            return res.status(400).json({success:false, error: err})
-        }
-
-        if (!recipe) {
-            return res
-                .status(404)
-                .json({success:false, error: `Recipe not found`})
-        }
-        return res.status(200).json({success:true, data: recipe})
-    })
-}
-
-getAllRecipes = async (req, res) => {
-    await Recipe.find({}, (err, recipes) => {
-        if(err){
-            return res.status(400).json({success: false, error: err})
-        }
-        if (!recipes.length){
-            return res
-                .status(404)
-                .json({ success: false, error: `Recipe not found`})
-        }
-        return res.status(200).json({ success: true, data: recipes })
-    }).catch(err => console.log(err))
-}
