23ff5a0c85064de41476eb6a62fba5c54f0c2d35
toki monkey <tokithemonkey@gmail.com>
2020-03-07 19:10:03 +0900

    test files

--- api/test/index.js
+++ api/test/index.js
@@ -3,5 +3,5 @@ const db = require('./db')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 module.exports = (req, res) => {
-    return res.status(200).json({ success: true, message: "Hey"})
+    return res.status(200).json({ success: true, message: "Hey from test"})
 }
